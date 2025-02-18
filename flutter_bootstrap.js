0a5a13aee9d2e1defac870033d1c77ab903da0fc
xuanthai-work <xuanthai.bxt@gmail.com>
2025-02-18 23:08:35 +0700

    add donate pic

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,10 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-<<<<<<< HEAD
-    serviceWorkerVersion: "2179199949"
-=======
-    serviceWorkerVersion: "2353119491"
->>>>>>> 74a9882 (donate pic)
+    serviceWorkerVersion: "2887030929"
   }
 });
