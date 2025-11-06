b125c9c64f0c61bfd05a362c133f983d468586c1
xuanthai-work <xuanthai.bxt@gmail.com>
2025-11-06 22:28:01 +0700

    sửa logic phát nhạc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "689271144"
+    serviceWorkerVersion: "206262235"
   }
 });
